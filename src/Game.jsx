e4c8a646cd8abd60af17865bf736f7bd9de29fda
JagTheFriend <JagTheFriend12@gmail.com>
2025-03-22 10:33:25 +0530

    refactor: enhance Game component logic for falling bottles and rendering
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,26 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { PiBeerBottleFill } from "react-icons/pi";
 
+const colors = ["#FF3131", "#FFFF00", "#0FFF50", "#FF800D", "#00F0FF"];
+
 export default function Game() {
-  const [counter, setCounter] = useState(-1);
-  const colors = ["#FF3131", "#FFFF00", "#0FFF50", "#FF800D", "#00F0FF"];
+  const [fallingBottle, setFallingBottle] = useState(null);
+  const [removedBottles, setRemovedBottles] = useState([]);
+  const cycleCount = useRef(0);
+  const startingTime = useRef();
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * 3);
+    setTimeout(() => {
+      setFallingBottle(randomIndex);
+      startingTime.current = Date.now();
+
+      setTimeout(() => {
+        setRemovedBottles([...removedBottles, randomIndex]);
+        setFallingBottle(null);
+      }, 2000);
+    }, 2000);
+  }, []);
 
   return (
     <div className="flex flex-row items-center justify-center w-full gap-[15%]">
       {Array(3)
         .fill(0)
-        .map((_, i) => (
-          <PiBeerBottleFill
-            className={`text-[150px] transition-transform duration-[2000ms] ${
-              counter === i ? "translate-y-[120vh]" : ""
-            }`}
-            style={{
-              color: colors[Math.floor(Math.random() * colors.length)],
-              position: "relative",
-            }}
-            key={`${12}-${i}`}
-          ></PiBeerBottleFill>
-        ))}
+        .map((_, i) =>
+          removedBottles.includes(i) ? null : (
+            <PiBeerBottleFill
+              className={`text-[150px] transition-transform duration-[2000ms] ${
+                fallingBottle === i ? "translate-y-[120vh]" : ""
+              }`}
+              style={{
+                color: colors[Math.floor(Math.random() * colors.length)],
+                position: "relative",
+              }}
+              key={`${12}-${i}`}
+            ></PiBeerBottleFill>
+          )
+        )}
     </div>
   );
 }
