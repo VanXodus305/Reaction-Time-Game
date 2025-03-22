82e41e51b40b9c0ddd29de2f6d849e3446b73cda
JagTheFriend <JagTheFriend12@gmail.com>
2025-03-22 09:07:46 +0530

    implement login endpoint and add HTTP request example for testing
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,40 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.post("/auth/login", async (c) => {
+  const body = await c.req.json();
+  const { name, rollNo } = body;
+
+  if (!name || !rollNo) {
+    return c.json({ error: "Name and roll number are required." }, 400);
+  }
+});
+
+app.post("/time", async (c) => {
+  const body = await c.req.json();
+  const { time } = body;
+
+  if (!time) {
+    return c.json({ error: "Time is required." }, 400);
+  }
+
+  return c.json({ message: `Time received: ${time}` });
+});
+
+app.get("/leaderboard", async (c) => {});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
